f9cb3e912f03ba45ebeeac3a175460c813c6bc41
Isaac <isaac.isaac.henrique@gmail.com>
2019-05-13 11:33:31 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cp -r Android/ ~/
+
+mkdir -p ~/.android
+touch ~/.android/repositories.cfg
+
+sdkmanager=~/Android/Sdk/tools/bin/sdkmanager
+
+sdkmanager "build-tools;28.0.3" "platforms;android-28" "system-images;android-28;google_apis;x86_64" "sources;android-28"
+
+sleep 1
+
+sdkmanager --licenses
+
+# Export Path Variables in Bash or Zsh
+
+pathvar="
+export ANDROID_HOME=~/Android/Sdk
+export PATH=\$PATH:\$ANDROID_HOME/tools/bin
+export PATH=\$PATH:\$ANDROID_HOME/platform-tools
+"
+
+if [-f file="~/.zshrc"] then
+	pathvar >> ~/.zshrc
+	source ~/.zshrc
+else
+	pathvar >> ~/.bashrc
+	source ~/.bashrc
+fi
+
